cafbf2fef00344530924821ad0b8ef8d300bfd68
Madison Levy <madisonlevy@Madisons-MacBook-Pro-5.local>
2026-02-14 17:12:04 -0700

    shortened label names

--- app.py
+++ app.py
@@ -107,6 +107,8 @@ if prompt:
     print(f"prompt {combined_text}")
     results_df, is_red_flag = re.get_results(combined_text, threshold)
     results_df= pd.DataFrame(results_df, columns=["Flag", "Scores"])
+
+
     if is_red_flag:
         response = "RED FLAGGGGGG 🚩🚩🚩🚩🚩🚩"   # Generate response to be implemented
     else:
@@ -123,7 +125,6 @@ if prompt:
     # Display Results
     with st.sidebar:
         st.divider()
-
         st.header("Results")
-        results_container = st.container()
+        results_container = st.container() 
         results_table = st.table(results_df)
